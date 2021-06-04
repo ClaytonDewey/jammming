864aaf4b7a10fc9799a9c29614706b6d79edaefa
Clayton Dewey <clay@dryadmedia.com>
2021-06-04 15:35:45 -0500

    Update CSS

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
         super(props);
         this.state = {
             searchResults: [],
-            playlistName: "Jammming Playlist!!",
+            playlistName: "New Playlist",
             playlistTracks: [],
         };
 
